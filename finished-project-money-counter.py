c4e8816db20c4b01b750777dccac2fb12c74b5bf
prothepro1234 <66332641+prothepro1234@users.noreply.github.com>
2020-09-29 12:19:05 -0400

    first commit

---
+++ finished-project-money-counter.py
@@ -0,0 +1,12 @@
+name = input('\n\n\n\nWhat do you want to buy: ') # Ask them what they want to buy
+num = int(input("How many do you want to buy: ")) # Ask them how much
+cost = int(input('How much does it cost: ')) # Ask them how much it cost
+
+tax = 0.14975 # Tax number in Canada
+totalnotax = num*cost # Total that the tax is not included
+taxplustotal = ((total * (1 + tax)) - total # Tax
+totalplustax = totalnotax + aaa # Total plus tax
+
+print('Tax: ',taxplustotal,'$') # Tell them tax number
+print('Object cost: ',totalnotax,'$') # Tell them object cost
+print('Total: ',totalplustax,'$') # Tell them the total
