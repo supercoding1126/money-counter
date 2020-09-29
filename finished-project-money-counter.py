1e792d47102926c76d3c16131a9f40742acd5308
supercoding1126 <66332641+supercoding1126@users.noreply.github.com>
2020-09-29 12:28:15 -0400

    Update finished-project-money-counter.py

--- finished-project-money-counter.py
+++ finished-project-money-counter.py
@@ -1,3 +1,9 @@
+# Money conting machine
+# By SuperCoding
+# Python 3.8.5
+# Mac os
+
+
 name = input('\n\n\n\nWhat do you want to buy: ') # Ask them what they want to buy
 num = int(input("How many do you want to buy: ")) # Ask them how much
 cost = int(input('How much does it cost: ')) # Ask them how much it cost
